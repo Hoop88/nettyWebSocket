dde5982e5adaa2c81b40aa65b39605fe729e07e9
Edson.di <liudi7799@163.com>
2019-04-19 11:15:50 +0800

    SpringBoot 注入bean到netty 启动类
    SpringBoot 注入bean到netty 启动类

--- src/main/java/com/websocket/netty/nettyboot/nettyboot/NettybootApplication.java
+++ src/main/java/com/websocket/netty/nettyboot/nettyboot/NettybootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.annotation.Resource;
 
+
 @SpringBootApplication
 public class NettybootApplication implements CommandLineRunner {
 	@Resource
